1194dff18161033e70b5c15b69d0002344f4d249
mjdierkes <mjdierkes@msn.com>
2022-05-15 10:29:16 -0500

    Initial Commit

---
+++ Kernel/KernelApp.swift
@@ -0,0 +1,17 @@
+//
+//  KernelApp.swift
+//  Kernel
+//
+//  Created by Mason Dierkes on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct KernelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
